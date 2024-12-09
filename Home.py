06e57ad2d1f3c1254a6dd1ba4e86a44f10bebe9f
Rosen Ruev <rosenruev@gmail.com>
2024-12-09 22:30:38 +0100

    test19

--- Home.py
+++ Home.py
@@ -3,7 +3,10 @@ import streamlit as st
 from helpers import ee_authenticate
 
 st.set_page_config(layout="wide")
-ee_authenticate("json_data", "service_account")
+
+json_data = st.secrets["json_data"]
+service_account = st.secrets["service_account"]
+ee_authenticate(json_data, service_account)
 
 ## Home page view
 # TODO  
